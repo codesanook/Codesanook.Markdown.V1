0df1fb8ff4885318b1a20d487787bf0c8c490ce8
aaronamm <theeranitp@gmail.com>
2020-04-30 09:22:31 +0700

    Add required components for Codesanook.Markdown module

---
+++ Migrations.cs
@@ -0,0 +1,43 @@
+﻿//https://google.github.io/styleguide/javaguide.html#s3-source-file-structure
+
+using System.Linq;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Data.Migration;
+
+namespace Markdown {
+
+    public class Migrations : DataMigrationImpl {
+        private readonly IContentDefinitionManager contentDefinitionManager;
+
+        public Migrations(IContentDefinitionManager contentDefinitionManager) =>
+            this.contentDefinitionManager = contentDefinitionManager;
+
+        public int Create() {
+            if (DoesBodyPartOfBlogPostTypeExist()) {
+                SetEditorToMarkdown();
+            }
+
+            return 1;
+        }
+
+        private bool DoesBodyPartOfBlogPostTypeExist() {
+            var blogPost = contentDefinitionManager.GetTypeDefinition("BlogPost");
+            if (blogPost == null) return false;
+
+            var bodyPart = blogPost.Parts.SingleOrDefault(x => x.PartDefinition.Name == "BodyPart");
+            return bodyPart != null;
+        }
+
+        private void SetEditorToMarkdown() {
+            contentDefinitionManager.AlterTypeDefinition(
+                "BlogPost",
+                build => build.WithPart(
+                    "BodyPart",
+                    // Change setting of content type
+                    // https://docs.orchardproject.net/en/latest/Documentation/Adding-custom-settings/#defining-settings-for-content-types
+                    cfg => cfg.WithSetting("BodyTypePartSettings.Flavor", "markdown")
+                )
+            );
+        }
+    }
+}
---
+++ Services/MarkdownFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Orchard.Services;
+
+namespace Codesanook.Markdown.Services {
+
+    public class MarkdownFilter : IHtmlFilter {
+
+        public string ProcessContent(string text, string flavor) =>
+            string.Equals(flavor, "markdown", StringComparison.OrdinalIgnoreCase)
+                ? MarkdownReplace(text)
+                : text;
+
+        private static string MarkdownReplace(string text) {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+
+            var markdown = new MarkdownSharp.Markdown();
+            return markdown.Transform(text);
+        }
+    }
+}
