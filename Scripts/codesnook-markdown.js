0efd0a94ab71c8691342d359a86d52dce6b0cb06
neuroanas <anas.hajeechiming@gmail.com>
2020-05-12 22:56:16 +0700

    Delete jquery and adjust function add link with highlight text and editor tool and pre code

--- Scripts/codesnook-markdown.js
+++ Scripts/codesnook-markdown.js
@@ -1,130 +1,113 @@
-(function () {
-
-    var easyMDE = new EasyMDE({
-        element: $('.wmd-input')[0],
-        autoDownloadFontAwesome: false,
-        spellChecker: false,
-        renderingConfig: {
-            singleLineBreaks: true,
-            codeSyntaxHighlighting: true,
+var easyMDE = new EasyMDE({
+    autoDownloadFontAwesome: false,
+    spellChecker: false,
+    renderingConfig: {
+        singleLineBreaks: true,
+        codeSyntaxHighlighting: true,
+    },
+    promptURLs: true,
+    toolbar: [
+        'bold',
+        'italic',
+        'heading',
+        '|',
+        'quote',
+        'unordered-list',
+        'ordered-list',
+        '|',
+        {
+            name: 'link',
+            action: (editor) => {
+                var codemirror = editor.codemirror;
+                var stat = editor.getState(cm);
+                var options = editor.options;
+                var url = 'https://';
+                if (options.promptURLs) {
+                    url = prompt(options.promptTexts.link, 'https://');
+                    if (!url) {
+                        return false;
+                    }
+                }
+                replaceSelection(codemirror, stat.link, options.insertTexts.link, url);
+            },
+            className: 'fa fa-link',
+            title: 'Create Link',
         },
-        promptURLs: true,
-        toolbar: [
-            'bold',
-            'italic',
-            'strikethrough',
-            'heading',
-            'heading-smaller',
-            'heading-bigger',
-            'heading-1',
-            'heading-2',
-            'heading-3',
-            '|',
-            'code',
-            'quote',
-            'unordered-list',
-            'ordered-list',
-            'clean-block',
-            '|',
-            {
-                name: 'link',
-                action: (editor) => {
-                    var cm = editor.codemirror;
-                    var stat = editor.getState(cm);
-                    var options = editor.options;
-                    var url = 'https://';
-                    if (options.promptURLs) {
-                        url = prompt(options.promptTexts.link, 'https://');
-                        if (!url) {
-                            return false;
+        {
+            name: 'link-image',
+            action: (editor) => {
+                var wmd = $('#wmd-input-Body');
+                var adminIndex = location.href.toLowerCase().indexOf("/admin/");
+                if (adminIndex === -1) return;
+                var url = location.href.substr(0, adminIndex) + "/Admin/Orchard.MediaLibrary?dialog=true";
+                $.colorbox({
+                    href: url,
+                    iframe: true,
+                    reposition: true,
+                    width: "90%",
+                    height: "90%",
+                    onLoad: function () {
+                        // hide the scrollbars from the main window
+                        $('html, body').css('overflow', 'hidden');
+                    },
+                    onClosed: function () {
+                        $('html, body').css('overflow', '');
+
+                        var selectedData = $.colorbox.selectedData;
+
+                        if (selectedData == null) // Dialog cancelled, do nothing
+                            return;
+
+                        var newContent = '';
+                        for (var i = 0; i < selectedData.length; i++) {
+                            var renderMedia = location.href.substr(0, adminIndex) + "/Admin/Orchard.MediaLibrary/MediaItem/" + selectedData[i].id + "?displayType=Raw";
+                            $.ajax({
+                                async: false,
+                                type: 'GET',
+                                url: renderMedia,
+                                success: function (data) {
+                                    newContent += data;
+                                }
+                            });
+                        }
+
+                        var result = $.parseHTML(newContent);
+                        var img = $(result).filter('img');
+                        // if this is an image, use the callback which will format it in markdown
+                        if (img.length > 0 && img.attr('src')) {
+
+                            var codemirror = editor.codemirror;
+                            var stat = editor.getState(cm);
+                            var options = editor.options;
+                            replaceSelection(codemirror, stat.image, options.insertTexts.image, img.attr('src'));
+                        }
+
+                        // otherwise, insert the raw HTML
+                        else {
+                            if (wmd.selection) {
+                                wmd.selection.replace('.*', newContent);
+                            } else {
+                                wmd.text(newContent);
+                            }
+                            callback();
                         }
                     }
-                    _replaceSelection(cm, stat.link, options.insertTexts.link, url);
-                },
-                className: 'fa fa-link',
-                title: 'Create Link',
+                });
             },
-            {
-                name: 'link-image',
-                action: (editor) => {
-                    var wmd = $('#wmd-input-Body');
-                    var adminIndex = location.href.toLowerCase().indexOf("/admin/");
-                    if (adminIndex === -1) return;
-                    var url = location.href.substr(0, adminIndex) + "/Admin/Orchard.MediaLibrary?dialog=true";
-                    $.colorbox({
-                        href: url,
-                        iframe: true,
-                        reposition: true,
-                        width: "90%",
-                        height: "90%",
-                        onLoad: function () {
-                            // hide the scrollbars from the main window
-                            $('html, body').css('overflow', 'hidden');
-                        },
-                        onClosed: function () {
-                            $('html, body').css('overflow', '');
+            className: 'fa fa-image',
+            title: 'Import Image',
 
-                            var selectedData = $.colorbox.selectedData;
+        },
+        '|',
+        'preview',
+        'side-by-side',
+        'fullscreen',
+        '|',
+        'guide',
+    ]
+});
 
-                            if (selectedData == null) // Dialog cancelled, do nothing
-                                return;
-
-                            var newContent = '';
-                            for (var i = 0; i < selectedData.length; i++) {
-                                var renderMedia = location.href.substr(0, adminIndex) + "/Admin/Orchard.MediaLibrary/MediaItem/" + selectedData[i].id + "?displayType=Raw";
-                                $.ajax({
-                                    async: false,
-                                    type: 'GET',
-                                    url: renderMedia,
-                                    success: function (data) {
-                                        newContent += data;
-                                    }
-                                });
-                            }
-
-                            var result = $.parseHTML(newContent);
-                            var img = $(result).filter('img');
-                            // if this is an image, use the callback which will format it in markdown
-                            if (img.length > 0 && img.attr('src')) {
-
-                                var cm = editor.codemirror;
-                                var stat = editor.getState(cm);
-                                var options = editor.options;
-                                _replaceSelection(cm, stat.image, options.insertTexts.image, img.attr('src'));
-                            }
-
-                            // otherwise, insert the raw HTML
-                            else {
-                                if (wmd.selection) {
-                                    wmd.selection.replace('.*', newContent);
-                                } else {
-                                    wmd.text(newContent);
-                                }
-                                callback();
-                            }
-                        }
-                    });
-                },
-                className: 'fa fa-image',
-                title: 'Import Image',
-
-            },
-            'table',
-            'horizontal-rule',
-            '|',
-            'preview',
-            'side-by-side',
-            'fullscreen',
-            '|',
-            'guide',
-            '|',
-            'undo',
-            'redo',
-        ]
-    });
-})();
-
-function _replaceSelection(cm, active, startEnd, url) {
+function replaceSelection(codemirror, active, startEnd, url) {
 
 
     var linkdescription = "enter link description here";
@@ -145,22 +128,23 @@ function _replaceSelection(cm, active, startEnd, url) {
         end = end.replace('#url#', url);
     }
     if (active) {
-        text = cm.getLine(startPoint.line);
+        text = codemirror.getLine(startPoint.line);
         start = text.slice(0, startPoint.ch) + (isImage ? imagedescription : linkdescription);
         end = text.slice(startPoint.ch);
-        cm.replaceRange(start + end, {
+        codemirror.replaceRange(start + end, {
             line: startPoint.line,
             ch: 0,
         });
     } else {
-        text = cm.getSelection();
-        console.log(start);
-        if (isImage) {
-            start = '![' + imagedescription+'](';
-        } else {
-            start = '[' + linkdescription;
+        text = codemirror.getSelection();
+        if (!text) {
+            if (isImage) {
+                start = '![' + imagedescription + '](';
+            } else {
+                start = '[' + linkdescription;
+            }
         }
-        cm.replaceSelection(start + text + end);
+        codemirror.replaceSelection(start + text + end);
 
         startPoint.ch += start.length;
         if (startPoint !== endPoint) {
