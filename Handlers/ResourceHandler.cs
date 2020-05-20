6b2b87d387ea8629d0e71779e6f9d559d1c18bc6
aaronamm <theeranitp@gmail.com>
2020-05-20 15:08:13 +0700

    Add syntax highlight to frontend

---
+++ Handlers/ResourceHandler.cs
@@ -0,0 +1,28 @@
+﻿using Orchard.ContentManagement.Handlers;
+using Orchard.Core.Common.Models;
+using Orchard.Environment;
+using Orchard.UI.Resources;
+
+namespace Codesanook.Markdown.Handlers {
+    public class ResourceHandler : ContentHandler {
+        private readonly Work<IResourceManager> resourceManager;
+
+        public ResourceHandler(Work<IResourceManager> resourceManager) =>
+            this.resourceManager = resourceManager;
+
+        protected override void BuildDisplayShape(BuildDisplayContext context) {
+            if (
+                context.ContentItem.ContentType == "BlogPost" &&
+                context.ContentItem.Has(typeof(BodyPart)) &&
+                context.DisplayType == "Detail"
+            ){
+                resourceManager.Value.Require("stylesheet", "HighlightJS").AtHead();
+                resourceManager.Value.Require("script", "HighlightJS").AtHead();
+                resourceManager.Value.Require("script", "HighlightJSInitialization").AtHead();
+            }
+
+            base.BuildDisplayShape(context);
+        }
+    }
+}
+
--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -4,6 +4,9 @@ namespace Markdown {
     public class ResourceManifest : IResourceManifestProvider {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
+            manifest.DefineStyle("HighlightJS").SetUrl("darcula.min.css");
+            manifest.DefineScript("HighlightJS").SetUrl("highlight.min.js");
+            manifest.DefineScript("HighlightJSInitialization").SetUrl("highlight-initialization.js");
         }
     }
 }
